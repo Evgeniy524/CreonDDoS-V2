d74af7d8512dc6c028d08771564d5e00a97b2aef
Lam Tin <ngolamtinzk@gmail.com>
2023-07-07 22:12:36 +0700

    Update indexHandler.js

--- src/modules/handlers/indexHandler.js
+++ src/modules/handlers/indexHandler.js
@@ -11,24 +11,23 @@ module.exports = async (client) => {
     const commandArray = [];
 
     fs.readdirSync("./src/events/").forEach(async (dir) => {
-        const events = fs.readdirSync(`./src/events/${dir}`).filter(f => f.endsWith(".js"));
+        const events = fs.readdirSync(path.join("./src/events/", dir)).filter(f => f.endsWith(".js"));
 
         for (const file of events) {
-            const event = require(path.resolve("src\\events\\" + dir, file));
-            
+            const event = require(path.resolve("src/events", dir, file));
             if (event.name) {
                 client.on(event.name, (...args) => event.execute(...args, client));
-                delete require.cache[require.resolve(path.resolve("src\\events\\" + dir, file))];
+                delete require.cache[require.resolve(path.resolve("src/events", dir, file))];
             } else {
                 throw new Error("ailed to load event&8: " + `${file.split('.')[0]}`);
             }
         }
     });
 
-    const cmds = fs.readdirSync(`./src/commands/`).filter(f => f.endsWith('.js'));
+    const cmds = fs.readdirSync("./src/commands/").filter(f => f.endsWith('.js'));
 
     for (let file of cmds) {
-        const slashCmd = require(path.resolve("src\\commands\\", file));
+        const slashCmd = require(path.resolve("src/commands", file));
 
         if (!slashCmd.data?.name) return;
         if (["MESSAGE", "USER"].includes(slashCmd.type)) delete file.data?.description;
